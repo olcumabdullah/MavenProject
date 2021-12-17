4a50f504d542c08f0e7ffa5444406a86c416916d
Abdullah Olcum <abdullaholcum123456@gmail.com>
2021-12-17 10:31:00 +0100

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to github");
+        System.out.println("Doing more work");
     }
 
 
